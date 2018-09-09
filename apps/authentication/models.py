2f68ad481407a4713d9a8f37ca27b3d00b553627
unknown <jamesj0107@naver.com>
2018-09-09 23:43:21 +0900

    Add module_perms for User models, register on admin site

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import AbstractBaseUser
 
 from .managers import UserManager
+
 
 
 class User(AbstractBaseUser):
@@ -19,3 +21,11 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+admin.site.register(User)
