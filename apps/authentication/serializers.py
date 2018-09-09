df86dbb23814bfc9085522be721b5c7838534367
Pellstrike <pellstrike@gmail.com>
2018-09-09 23:21:30 +0900

    Remove UserSerializer in serializers.py

--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -12,23 +12,3 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
-
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['email', 'username', 'student_id', 'password']
-
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-
-        for (key, value) in validated_data.items():
-            setattr(instance, key, value)
-
-        if password is not None:
-            instance.set_password(password)
-
-        instance.save()
-        return instance
