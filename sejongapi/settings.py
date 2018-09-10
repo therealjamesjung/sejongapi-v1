5ca1aba8a3e95d34ee33c2ca3392a732a1e8ad08
unknown <jamesj0107@naver.com>
2018-09-10 16:44:29 +0900

    Update settings for JWT authentication

--- sejongapi/settings.py
+++ sejongapi/settings.py
@@ -138,5 +138,13 @@ django_heroku.settings(locals())
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
