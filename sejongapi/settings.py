475eaac6f4b8d0c121b881b3c5eae24e99d33e60
Pellstrike <pellstrike@gmail.com>
2018-09-11 00:15:03 +0900

    Update rest framework settings

--- sejongapi/settings.py
+++ sejongapi/settings.py
@@ -127,6 +127,16 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'authentication.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 
 JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
