2798f31bc48aab29a88a072d79714da6fe012a3c
unknown <jamesj0107@naver.com>
2018-09-11 02:41:20 +0900

    Create comment serializer

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -8,3 +8,9 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = '__all__'
         read_only_fields = ('writer',)
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('writer',)
