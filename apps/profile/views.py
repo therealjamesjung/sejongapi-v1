e918ccd61951a0325703e9957bf2d740b7029d4c
Pellstrike <pellstrike@gmail.com>
2018-09-11 00:15:20 +0900

    Create views for profile app

---
+++ apps/profile/views.py
@@ -0,0 +1,49 @@
+from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import RetrieveAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Profile
+from .serializers import ProfileSerializer
+
+
+class ProfileRetrieveAPIView(RetrieveAPIView):
+    queryset = Profile.objects.select_related('user')
+    permission_classes = (IsAuthenticated, )
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, user_pk=None, *args, **kwargs):
+        try:
+            profile = self.queryset.get(pk=user_pk)
+        except Profile.DoesNotExist:
+            raise NotFound('A profile with this username does not exist.')
+
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Profile.objects.select_related('user')
+    permission_classes = (IsAuthenticated, )
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            profile = self.queryset.get(user=request.user)
+        except Profile.DoesNotExist:
+            raise NotFound('This user does not have a profile.')
+
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            profile = self.queryset.get(user=request.user)
+        except Profile.DoesNotExist:
+            raise NotFound('This user does not have a profile.')
+
+        serializer = self.get_serializer(profile, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
