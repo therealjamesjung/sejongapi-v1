8f05657e60f340f84e30c3193beb0b41258313f4
unknown <jamesj0107@naver.com>
2018-09-10 16:43:57 +0900

    Create post seraializer

---
+++ apps/post/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.Serializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ('author', )
