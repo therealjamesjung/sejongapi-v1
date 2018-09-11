0bea5b75c2ce152d3f72f6ee3de2df02687b878d
Pellstrike <pellstrike@gmail.com>
2018-09-11 14:44:22 +0900

    Create channel model

--- apps/channel/models.py
+++ apps/channel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+    rules = models.TextField(blank=True)
+    moderators = models.ForeignKey('authentication.User', on_delete=models.CASCADE, related_name='moderators')
+
+    def __str__(self):
+        return self.name
+
+    def get_channel_rule(self, index):
+        rule_list = rules.split('\n')
+        return rule_list[index]
