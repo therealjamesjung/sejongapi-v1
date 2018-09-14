99a6e44a55dd6c4e8de729ac66f87b8beed8d256
Pellstrike <pellstrike@gmail.com>
2018-09-14 16:08:04 +0900

    Fix invalid relationship in channel model

--- apps/channel/models.py
+++ apps/channel/models.py
@@ -5,11 +5,7 @@ class Channel(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
     rules = models.TextField(blank=True)
-    moderators = models.ForeignKey('authentication.User', on_delete=models.CASCADE, related_name='moderators')
+    moderators = models.ManyToManyField('profile.Profile', related_name='moderators')
 
     def __str__(self):
         return self.name
-
-    def get_channel_rule(self, index):
-        rule_list = rules.split('\n')
-        return rule_list[index]
