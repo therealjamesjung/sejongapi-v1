30167752d2ece43955abf66dde8b16548aed4758
Pellstrike <pellstrike@gmail.com>
2018-09-14 16:09:16 +0900

    Fix channel views so that they use correct serializer

--- apps/channel/views.py
+++ apps/channel/views.py
@@ -1,16 +1,16 @@
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Channel
-from .serializers import ChannelSerializer
+from .serializers import ChannelRetrieveSerializer, ChannelCreateSerializer
 
 
 class ChannelCreateAPIView(CreateAPIView):
     permission_classes = (IsAuthenticated, )
-    serializer_class = ChannelSerializer
+    serializer_class = ChannelCreateSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -19,18 +19,19 @@ class ChannelCreateAPIView(CreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class ChannelListUpdateAPIView(ListAPIView, UpdateAPIView):
+class ChannelRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Channel.objects.all()
     permission_classes = (IsAuthenticated, )
-    serializer_class = ChannelSerializer
+    retrieve_serializer_class = ChannelRetrieveSerializer
+    update_serializer_class = ChannelCreateSerializer
 
-    def list(self, request, channel_pk=None, *args, **kwargs):
+    def retrieve(self, request, channel_pk=None, *args, **kwargs):
         try:
-            channel = Channel.objects.filter(pk=channel_pk)
+            channel = self.queryset.get(pk=channel_pk)
         except Channel.DoesNotExist:
             raise NotFound('A channel with this pk does not found.')
 
-        serializer = self.get_serializer(channel, many=True)
+        serializer = self.retrieve_serializer_class(channel)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, channel_pk=None, *args, **kwargs):
@@ -39,7 +40,7 @@ class ChannelListUpdateAPIView(ListAPIView, UpdateAPIView):
         except Channel.DoesNotExist:
             raise NotFound('A channel with this pk does not found.')
 
-        serializer = self.get_serializer(channel, data=request.data, partial=True)
+        serializer = self.update_serializer_class(channel, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
