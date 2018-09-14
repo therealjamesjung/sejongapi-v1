627b56b67783082c54d3586a7108a079dc0352a6
Pellstrike <pellstrike@gmail.com>
2018-09-14 16:08:39 +0900

    Update urls for channel app

--- apps/channel/urls.py
+++ apps/channel/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import ChannelCreateAPIView, ChannelListUpdateAPIView
+from .views import ChannelCreateAPIView, ChannelRetrieveUpdateAPIView
 
 app_name = 'channel'
 urlpatterns = [
     path('channels/', ChannelCreateAPIView.as_view()),
-    path('channels/<int:channel_pk>/', ChannelListUpdateAPIView.as_view()),
+    path('channels/<int:channel_pk>/', ChannelRetrieveUpdateAPIView.as_view()),
 ]
