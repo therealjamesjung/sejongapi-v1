0b3ba6f19d869178c048fa3ab3e9e4cb7a5b37c9
Pellstrike <pellstrike@gmail.com>
2018-09-16 20:30:17 +0900

    Adjust files to follow PEP8 styleguide

--- apps/post/views.py
+++ apps/post/views.py
@@ -7,28 +7,27 @@ from .models import Article, Comment
 from .serializers import ArticleSerializer, CommentSerializer
 from apps.channel.models import Channel
 
+
 class ArticleListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ArticleSerializer
     lookup_url_kwarg = 'channel_pk'
 
     def get_queryset(self):
         channel_pk = self.kwargs.get('channel_pk')
-        queryset = Article.objects.filter(channel_id = channel_pk)
+        queryset = Article.objects.filter(channel_id=channel_pk)
         if not queryset:
             raise NotFound('A Channel with this primary key does not exists')
         return queryset
 
     def create(self, request, *args, **kwargs):
         channel_pk = self.kwargs.get('channel_pk')
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(writer = request.user.profile, upvoted = None, downvoted = None, channel_id = channel_pk)
+        serializer.save(writer = request.user.profile, upvoted=None, downvoted=None, channel_id=channel_pk)
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
 
-
 class ArticleRetrieveUpdateDeleteCommentCreateAPIView(mixins.CreateModelMixin, generics.RetrieveUpdateDestroyAPIView):
-
     lookup_url_kwarg = 'article_pk'
 
     def get_serializer_class(self):
@@ -39,12 +38,12 @@ class ArticleRetrieveUpdateDeleteCommentCreateAPIView(mixins.CreateModelMixin, g
 
     def get_queryset(self):
         article_pk = self.kwargs.get('article_pk')
-        queryset = Article.objects.filter(id = article_pk)
+        queryset = Article.objects.filter(id=article_pk)
         if not queryset:
             raise NotFound('An Article with this primary key does not exists')
         return queryset
 
-    def update(self, request, article_pk = None, *args, **kwargs):
+    def update(self, request, article_pk=None, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         serializer_instance = self.get_object()
         if serializer_instance.writer_id == request.user.id:
@@ -59,17 +58,18 @@ class ArticleRetrieveUpdateDeleteCommentCreateAPIView(mixins.CreateModelMixin, g
         article_pk = self.kwargs.get('article_pk')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(writer = request.user.profile, post_id = article_pk)
+        serializer.save(writer=request.user.profile, post_id=article_pk)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def delete(self, request, article_pk = None, *args, **kwargs):
+    def delete(self, request, article_pk=None, *args, **kwargs):
         serializer_instance = self.get_object()
         if serializer_instance.writer_id == request.user.id:
             self.perform_destroy(serializer_instance)
-            return Response(status = status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             raise PermissionDenied('You are not authorized to delete this post')
+
 
 class CommentListAPIView(generics.ListAPIView):
     serializer_class = CommentSerializer
@@ -77,10 +77,11 @@ class CommentListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         article_pk = self.kwargs.get('article_pk')
-        queryset = Comment.objects.filter(post_id = article_pk)
+        queryset = Comment.objects.filter(post_id=article_pk)
         if not queryset:
             raise NotFound('There are no comments on this article.')
         return queryset
+
 
 class CommentUpdateDeleteAPIView(mixins.DestroyModelMixin, generics.UpdateAPIView):
     serializer_class = CommentSerializer
@@ -88,12 +89,12 @@ class CommentUpdateDeleteAPIView(mixins.DestroyModelMixin, generics.UpdateAPIVie
 
     def get_queryset(self):
         comment_pk = self.kwargs.get('comment_pk')
-        queryset = Comment.objects.filter(id = comment_pk)
+        queryset = Comment.objects.filter(id=comment_pk)
         if not queryset:
             raise NotFound('A Comment with this primary key does not exists')
         return queryset
 
-    def update(self, request, comment_pk = None, *args, **kwargs):
+    def update(self, request, comment_pk=None, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         serializer_instance = self.get_object()
         if serializer_instance.writer_id == request.user.id:
@@ -103,7 +104,8 @@ class CommentUpdateDeleteAPIView(mixins.DestroyModelMixin, generics.UpdateAPIVie
             return Response(serializer.data, status = status.HTTP_200_OK)
         else:
             raise PermissionDenied('You are not authorized to update this comment')
-    def delete(self, request, comment_pk = None, *args, **kwargs):
+
+    def delete(self, request, comment_pk=None, *args, **kwargs):
         serializer_instance = self.get_object()
         if serializer_instance.writer_id == request.user.id:
             self.perform_destroy(serializer_instance)
