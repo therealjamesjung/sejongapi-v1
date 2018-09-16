6ba6f09471e9fd49333aecafc1aa2901d523dfb1
Pellstrike <pellstrike@gmail.com>
2018-09-17 02:11:59 +0900

    Fix production settings

--- sejongapi/settings/production.py
+++ sejongapi/settings/production.py
@@ -12,10 +12,4 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_SSL_REDIRECT = True
 
-REST_FRAMEWORK += {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
-}
-
 django_heroku.settings(locals())
