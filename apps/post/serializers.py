da773eba71eb6299343dee61ca5a3b2b4c8b12f7
Pellstrike <pellstrike@gmail.com>
2018-09-16 20:30:49 +0900

    Adjust files to follow PEP8 styleguide

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -6,17 +6,18 @@ from .models import Article, Comment
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    writer = ProfileSerializer(read_only = True)
+    writer = ProfileSerializer(read_only=True)
 
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('writer', 'channel')
+        read_only_fields = ('writer', 'channel', )
+
 
 class CommentSerializer(serializers.ModelSerializer):
-    writer = ProfileSerializer(read_only = True)
+    writer = ProfileSerializer(read_only=True)
 
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('writer', 'post')
+        read_only_fields = ('writer', 'post', )
