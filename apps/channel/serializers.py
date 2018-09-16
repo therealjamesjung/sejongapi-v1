2b8cd0d9194fd92b2643d067421151f0ff807c54
Pellstrike <pellstrike@gmail.com>
2018-09-16 15:15:18 +0900

    Implement subscribers and blacklist for serializer

--- apps/channel/serializers.py
+++ apps/channel/serializers.py
@@ -7,22 +7,26 @@ from .models import Channel
 
 class ChannelCreateSerializer(serializers.ModelSerializer):
     moderators = ProfileSerializer(many=True, read_only=True)
+    subscribers = ProfileSerializer(many=True, read_only=True)
+    blacklist = ProfileSerializer(many=True, read_only=True)
 
     class Meta:
         model = Channel
-        fields = ('name', 'description', 'rules', 'moderators', )
+        fields = ('name', 'description', 'rules', 'moderators', 'subscribers', 'blacklist', )
 
 
 class ChannelRetrieveSerializer(serializers.ModelSerializer):
     moderators = ProfileSerializer(many=True)
+    subscribers = ProfileSerializer(many=True)
+    blacklist = ProfileSerializer(many=True)
 
     class Meta:
         model = Channel
-        fields = ('name', 'description', 'rules', 'moderators', )
+        fields = ('name', 'description', 'rules', 'moderators', 'subscribers', 'blacklist', )
 
         
 class ChannelUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
-        fields = ('name', 'description', 'rules', 'moderators', )
+        fields = ('name', 'description', 'rules', 'moderators', 'subscribers', 'blacklist', )
 
