19b7762c107cf6f8d1ee3620e701a85138cd94f6
Pellstrike <pellstrike@gmail.com>
2018-09-16 15:15:00 +0900

    Add subscribers and blacklist to the channel app

--- apps/channel/models.py
+++ apps/channel/models.py
@@ -5,7 +5,10 @@ class Channel(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
     rules = models.TextField(blank=True)
+
     moderators = models.ManyToManyField('profile.Profile', related_name='moderators')
+    subscribers = models.ManyToManyField('profile.Profile', related_name='subscribers')
+    blacklist = models.ManyToManyField('profile.Profile', related_name='blacklist')
 
     def __str__(self):
         return self.name
