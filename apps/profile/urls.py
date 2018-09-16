887112738b1761adf5928ed0764dbc1082b874ca
Pellstrike <pellstrike@gmail.com>
2018-09-16 20:23:23 +0900

    Fix regex format error in profile urls.py

--- apps/profile/urls.py
+++ apps/profile/urls.py
@@ -6,6 +6,6 @@ app_name = 'profile'
 urlpatterns = [
     path('profiles/', ProfileRetrieveUpdateAPIView.as_view()),
     path('profiles/<int:user_pk>/', ProfileRetrieveAPIView.as_view()),
-    path('profiles/<user_pk>/follow/',FollowUpdateAPIView.as_view()),
-    path('profiles/<user_pk>/unfollow/',UnfollowUpdateAPIView.as_view()),
+    path('profiles/<int:user_pk>/follow/', FollowUpdateAPIView.as_view()),
+    path('profiles/<int:user_pk>/unfollow/', UnfollowUpdateAPIView.as_view()),
 ]
