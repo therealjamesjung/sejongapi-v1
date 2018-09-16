857f49c52f5270dc63e9f6ff6dd0ab421989d468
Pellstrike <pellstrike@gmail.com>
2018-09-16 19:53:54 +0900

    Fix regex format in the post urls.py

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from .views import ArticleListCreateAPIView, ArticleRetrieveUpdateDeleteCommentCreateAPIView, CommentUpdateDeleteAPIView, CommentListAPIView
 
 urlpatterns = [
-    path('channels/<channel_pk>/posts/', ArticleListCreateAPIView.as_view()),
-    path('channels/<channel_pk>/posts/<article_pk>/', ArticleRetrieveUpdateDeleteCommentCreateAPIView.as_view()),
-    path('channels/<channel_pk>/posts/<article_pk>/comments/', CommentListAPIView.as_view()),
-    path('channels/<channel_pk>/posts/<article_pk>/comments/<comment_pk>/', CommentUpdateDeleteAPIView.as_view()),
+    path('channels/<int:channel_pk>/posts/', ArticleListCreateAPIView.as_view()),
+    path('channels/<int:channel_pk>/posts/<int:article_pk>/', ArticleRetrieveUpdateDeleteCommentCreateAPIView.as_view()),
+    path('channels/<int:channel_pk>/posts/<int:article_pk>/comments/', CommentListAPIView.as_view()),
+    path('channels/<int:channel_pk>/posts/<int:article_pk>/comments/<int:comment_pk>/', CommentUpdateDeleteAPIView.as_view()),
 ]
