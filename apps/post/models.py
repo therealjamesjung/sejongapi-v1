2fe62780d7abde9b581810421e78352a7f5a1368
Pellstrike <pellstrike@gmail.com>
2018-09-17 15:35:39 +0900

    Change article model so that they return total number of comments

--- apps/post/models.py
+++ apps/post/models.py
@@ -7,8 +7,8 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
 
-    upvoted = models.ManyToManyField('profile.Profile', blank=True)
-    downvoted = models.ManyToManyField'profile.Profile', blank=True)
+    upvoted = models.ManyToManyField('profile.Profile', blank=True, related_name='upvoted')
+    downvoted = models.ManyToManyField('profile.Profile', blank=True, related_name='downvoted')
 
     channel = models.ForeignKey('channel.Channel', on_delete=models.CASCADE)
 
@@ -17,9 +17,12 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+    def get_number_of_comments(self):
+        return self.comments.count()
+
 
 class Comment(models.Model):
     writer = models.ForeignKey('profile.Profile', on_delete=models.CASCADE)
     content = models.TextField()
-    post = models.ForeignKey('post.Article', on_delete=models.CASCADE)
-    parent = models.ForeignKey('post.Comment', on_delete=models.CASCADE, blank=True, null=True)
+    post = models.ForeignKey('post.Article', on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('post.Comment', on_delete=models.CASCADE, blank=True, null=True, related_name='childs')
